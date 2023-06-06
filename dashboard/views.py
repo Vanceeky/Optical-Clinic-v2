3091a657c5ce0096c69e06b54074485f46f8f02e
Vanceeky <94431639+Vanceeky@users.noreply.github.com>
2023-06-07 04:07:21 +0800

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -326,7 +326,6 @@ def product_list(request):
     }
     return render(request, 'dashboard/product_list.html', context)
 
-@allowed_users(allowed_roles=['admin', 'staff'])
 def add_category(request):
     if request.is_ajax() and request.method == 'POST':
         name = request.POST.get('name')
